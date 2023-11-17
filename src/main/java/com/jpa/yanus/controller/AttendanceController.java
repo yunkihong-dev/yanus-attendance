43f945e6c185489e46004d791235f925de4fa729
백엔드 개발자 홍윤기 <ricky0130@naver.com>
2023-11-17 20:47:53 +0900

    test

--- src/main/java/com/jpa/yanus/controller/AttendanceController.java
+++ src/main/java/com/jpa/yanus/controller/AttendanceController.java
@@ -22,32 +22,5 @@ import java.net.http.HttpRequest;
 @RequestMapping("/attendance/*")
 public class AttendanceController {
 
-    @GetMapping("getIp")
-    @ResponseBody
-    public String getIp() throws Exception {
-        String ip = null;
-        HttpServletRequest request =
-                ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-
-        // IPv4 주소를 얻기 위한 헤더들을 확인합니다. IPv6 주소를 무시합니다.
-        String[] headersToCheck = {"X-Forwarded-For", "Proxy-Client-IP", "WL-Proxy-Client-IP",
-                "HTTP_CLIENT_IP", "HTTP_X_FORWARDED_FOR", "X-Real-IP",
-                "X-RealIP", "REMOTE_ADDR"};
-
-        for (String header : headersToCheck) {
-            ip = request.getHeader(header);
-            if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
-                // 첫 번째로 찾은 유효한 IPv4 주소를 반환합니다.
-                log.info(ip);
-                return ip;
-            }
-        }
-
-        // 모든 헤더에서 유효한 IPv4 주소를 찾지 못한 경우, 기본적으로 사용자의 IP 주소를 반환합니다.
-        ip = request.getRemoteAddr();
-        log.info(ip);
-        return ip; 
-
-    }
 
 }
