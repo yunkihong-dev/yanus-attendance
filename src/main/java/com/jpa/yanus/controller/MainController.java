858bce0cb7916ac68488bfdd68468e8fec0394c8
백엔드 개발자 홍윤기 <ricky0130@naver.com>
2023-12-11 14:09:20 +0900

    main컨 수정

--- src/main/java/com/jpa/yanus/controller/MainController.java
+++ src/main/java/com/jpa/yanus/controller/MainController.java
@@ -1,17 +1,11 @@
 package com.jpa.yanus.controller;
 
-import com.jpa.yanus.domain.MemberDTO;
-import com.jpa.yanus.entity.Member;
 import com.jpa.yanus.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
-
-import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -22,13 +16,7 @@ public class MainController {
     private final MemberService memberService;
 
     @GetMapping("")
-    public String goMain(@SessionAttribute(name = "id", required = false) Long id, Model model){
-        final Optional<Member> foundMember = memberService.getMemberById(id);
-        if (foundMember.isPresent()){
-            model.addAttribute("member", new MemberDTO());
-            return "main";
-        }else{
-            return "main";
-        }
+    public String goMain(){
+        return "main";
     }
 }
