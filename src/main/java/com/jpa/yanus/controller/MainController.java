37e09c1d58bf204d2e80afafe5464cb8a4a588ae
백엔드 개발자 홍윤기 <ricky0130@naver.com>
2023-12-13 17:41:20 +0900

    이미지로 메인페이지 가기 추가

--- src/main/java/com/jpa/yanus/controller/MainController.java
+++ src/main/java/com/jpa/yanus/controller/MainController.java
@@ -1,11 +1,17 @@
 package com.jpa.yanus.controller;
 
+import com.jpa.yanus.entity.Member;
 import com.jpa.yanus.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -16,7 +22,22 @@ public class MainController {
     private final MemberService memberService;
 
     @GetMapping("")
-    public String goMain(){
-        return "main";
+    public String goMain(HttpSession session, Model model){
+
+        Long memberId = (Long) session.getAttribute("id");
+
+        if(memberId == null){
+            return "main";
+        } else {
+            Optional<Member> member = memberService.getMemberById(memberId);
+            if(member.isPresent()) {
+                model.addAttribute("member", member.get());
+            } else {
+                // member가 없으면 null을 모델에 추가하거나, 다른 처리를 할 수 있음
+                model.addAttribute("member", null);
+            }
+            return "main";
+        }
     }
+
 }
